bb7e2976b81c17536fdf8d719aeece00efdbadb2
zcaliptium <zcaliptium@gmail.com>
2017-12-08 00:03:50 +0300

    Add basic code and resources.

---
+++ src/main/java/tws/zcaliptium/knucklepay/common/KnucklePay.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2017 ZCaliptium.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
+package tws.zcaliptium.knucklepay.common;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ModInfo.MODID, name = ModInfo.MODNAME, version = ModInfo.VERSION)
+public class KnucklePay
+{   
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
